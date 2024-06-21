a304181ce1bfc54818f0a8417a38f3cf4898efef
AVTG <83997074+AVTG@users.noreply.github.com>
2024-06-21 12:45:29 +0530

    i

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -47,7 +47,7 @@ export const Home = () => {
 
 
             {/* Photo gallery */}
-            <div className="flex flex-wrap w-[100%] items-start justify-center gap-[12px_12px] p-[120px] relative  bg-[#f0efef] rounded-[30px] overflow-hidden">
+            {/* <div className="flex flex-wrap w-[100%] items-start justify-center gap-[12px_12px] p-[120px] relative  bg-[#f0efef] rounded-[30px] overflow-hidden">
                 <div className="relative flex-1 min-w-[411px] grow h-[300px] bg-[#f7f7f7] rounded-[10px] overflow-hidden">
                     <div className="left-[129px] absolute h-[29px] top-[135px] opacity-25 [font-family:'Inter',Helvetica] font-bold text-black text-[24px] text-center tracking-[-0.96px] leading-[normal]">
                         DESCRIPTION
@@ -103,7 +103,7 @@ export const Home = () => {
                         DESCRIPTION
                     </div>
                 </div>
-            </div>
+            </div> */}
 
             <FAQs />
 
