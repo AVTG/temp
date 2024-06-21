3441f1566604a7f7b287ccb6f0917f89ddd1f1ab
AVTG <83997074+AVTG@users.noreply.github.com>
2024-06-21 21:13:45 +0530

    .

--- src/pages/Home/components/ImageGallery.jsx
+++ src/pages/Home/components/ImageGallery.jsx
@@ -10,14 +10,14 @@ export const ImageGallery = () => {
                 Mementoes
             </div>
 
-            <Slide indicators={true} arrows={window.innerWidth > 500} autoplay={true} pauseOnHover={true}>
+            <Slide indicators={true} arrows = {window.innerWidth > 500 } autoplay={true} pauseOnHover={true}>
                 {
                     Data.SportsData.filter(object => {
                         return object.id > 0
                     }).map((object) => {
                         return object.SportsImage.map((image, index) => {
                             return (
-                                <div className="image-container w-[80%] aspect-[2/1] overflow-hidden object-fit pl-1 m-auto rounded-xl  backgroundSize: 'cover', backgroundPosition: 'center'">
+                                <div className={`image-container ${window.innerWidth > 600? "w-[85%]" : "w-[100%]" } aspect-[2/1] overflow-hidden object-fit pl-1 m-auto rounded-xl object-cover object-center`}>
                                     <img src={image} alt={`slide-${index}`} className="object-fit object-center" />
                                 </div>
                             )
